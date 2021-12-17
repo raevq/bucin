378a8a319a99e8ae8ded849314e574d2e68e1573
raevq <96299208+raevq@users.noreply.github.com>
2021-12-17 22:00:26 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,9 +1,9 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "standinshd", quotes: "I love you." },
+    { author: "A Midsummer Night's Dream – Act 1, Scene 1", quotes: "Love looks not with the eyes, but with the mind, And therefore is winged Cupid painted blind" },
+    { author: "The Tempest – Act 3, Scene 1", quotes: "Hear my soul speak. Of the very instant that I saw you, Did my heart fly at your service" },
+    { author: "King Lear – Act 1, Secene 1", quotes: "I love you more than words can wield the matter, dearer than eyesight, space and liberty" },
+    { author: "Hamlet – Act 2, Scene 2", quotes: "Doubt that the stars are fire, Doubt that the sun doth move his aides, Doubt truth to be a liar, But never doubt I love" },
     { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
     { author: "standinshd", quotes: "I love you." },
     { author: "standinshd", quotes: "I'm sorry for loving you." },
