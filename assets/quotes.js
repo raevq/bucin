ac7241202c3643efd8ff9947f9cd50773d079085
raevq <96299208+raevq@users.noreply.github.com>
2021-12-17 22:23:09 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -2,7 +2,7 @@
 export default [
     { author: "A Midsummer Night's Dream – Act 1, Scene 1", quotes: "Love looks not with the eyes, but with the mind, And therefore is winged Cupid painted blind." },
     { author: "The Tempest – Act 3, Scene 1", quotes: "Hear my soul speak. Of the very instant that I saw you, Did my heart fly at your service." },
-    { author: "King Lear – Act 1, Secene 1", quotes: "I love you more than words can wield the matter, dearer than eyesight, space and liberty." },
+    { author: "King Lear – Act 1, Scene 1", quotes: "I love you more than words can wield the matter, dearer than eyesight, space and liberty." },
     { author: "Hamlet – Act 2, Scene 2", quotes: "Doubt that the stars are fire, Doubt that the sun doth move his aides, Doubt truth to be a liar, But never doubt I love." },
     { author: "The Tempest – Act 3, Scene 1", quotes: "I would not wish any companion in the world but you." },
     { author: "Othello – Act 3, Scene 3", quotes: "I humbly do beseech of your pardon, For too much loving you." },
